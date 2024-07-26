bb2196a04f9ac41c8ace37b683fc71fe0452a40f
masc <59968873@qq.com>
2024-07-27 01:06:51 +0800

    点击全屏就横屏

--- app/src/main/java/com/masc/teriteri/MainActivity.java
+++ app/src/main/java/com/masc/teriteri/MainActivity.java
@@ -2,8 +2,12 @@ package com.masc.teriteri;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -43,6 +47,11 @@ public class MainActivity extends Activity {
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setUseWideViewPort(true); //是否可缩放
         webView.getSettings().setLoadWithOverviewMode(true);//加载自适应屏幕
+//        webView.getSettings().setAllowFullscreen(true);//加载自适应屏幕
+
+//        WebSettings webSettings = webView.getSettings();
+        webView.setWebViewClient(new MyWebViewClient());
+        webView.setWebChromeClient(new MyWebChromeClient());
 
     }
 
@@ -53,5 +62,38 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onKeyDown(keyCode, event);
+    }
+
+
+    private class MyWebViewClient extends WebViewClient {
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    private class MyWebChromeClient extends WebChromeClient {
+
+        @Override
+        public void onShowCustomView(View view, CustomViewCallback callback) {
+            Log.d("WebView", "Fullscreen requested");
+
+//            int originalUiVisibility = getWindow().getDecorView().getSystemUiVisibility();
+            // 设置为横屏
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            // 处理全屏显示的逻辑，比如隐藏状态栏、导航栏等
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        }
+
+        @Override
+        public void onHideCustomView() {
+            Log.d("WebView", "Fullscreen exited");
+            // 恢复为之前的屏幕方向
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
+            // 处理退出全屏的逻辑，恢复状态栏、导航栏等
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+        }
     }
 }
